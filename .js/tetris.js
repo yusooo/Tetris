4927c3be65c38067d0a42a35b8b5c27aab3025d1
yusooo <yusaei1107@gmail.com>
2022-06-27 22:28:42 +0900

    Base_Complete

--- .js/tetris.js
+++ .js/tetris.js
@@ -1,6 +1,12 @@
+import BLOCKS from "./blocks.js"
+
 // DOM
 
 const playground = document.querySelector(".playground > ul");
+const gameText = document.querySelector(".game-Text");
+const scoreDisplay = document.querySelector(".score");
+const restartButton = document.querySelector(".game-text > button")
+
 
 // Setting
 const GAME_ROWS = 20;
@@ -12,17 +18,8 @@ let duration = 500;
 let downInterval;
 let tempMovingItem;
 
-const BLOCKS = {
-    tree: [
-        [[2,1], [0,1], [1,0], [1,1]], 
-        [[1,2], [0,1], [1,0], [1,1]],
-        [[1,2], [0,1], [2,1], [1,1]],
-        [[2,1], [1,2], [1,0], [1,1]],
-    ]
-}
-
 const MovingItem = {
-    type: "tree",
+    type: "",
     direction: 0,
     top: 0,
     left: 0,
@@ -36,7 +33,7 @@ function init(){
     for(let i=0; i<GAME_ROWS;i++){
        prependNewLine()
     }
-    renderBlocks();
+    generateNewBlock();
 }
 function prependNewLine(){
     const li = document.createElement("li");
@@ -48,13 +45,13 @@ function prependNewLine(){
     li.prepend(ul)
     playground.prepend(li)
 }
-function renderBlocks(){
+function renderBlocks(moveType=""){
     const { type, direction, top, left } = tempMovingItem;
     const movingBlocks = document.querySelectorAll(".moving");
     movingBlocks.forEach(moving => {
         moving.classList.remove(type, "moving");
     })
-    BLOCKS[type][direction].forEach(block=>{
+    BLOCKS[type][direction].some(block=>{
         const x = block[0] + left;
         const y = block[1] + top;
         const target = playground.childNodes[y] ? playground.childNodes[y].childNodes[0].childNodes[x] : null;
@@ -63,12 +60,17 @@ function renderBlocks(){
            target.classlist.add(type, "moving")
         } else {
             tempMovingItem = {...MovingItem}
+            if(moveType === 'retry'){
+                clearInterval(downInterval)
+                showGameoverText()
+            }
             setTimeout(()=>{
-                renderBlocks()
+                renderBlocks('retry')
                 if(moveType === "top"){
                     seizeBlock();
                 }
             }, 0)
+            return true;
         }
     }) 
     MovingItem.left = left;
@@ -76,10 +78,51 @@ function renderBlocks(){
     MovingItem.direction = direction;
 }
 function seizeBlock(){
-    console.log('seize block')
+
+    const movingBlocks = document.querySelectorAll(".moving");
+    movingBlocks.forEach(moving => {
+        moving.classList.remove("moving");
+        moving.classList.add("seized");
+    })
+    checkMatch()
+}
+function checkMatch(){
+    const childNodes = playground.childNodes;
+    childNodes.forEach(child=>{
+        let matched = true;
+        child.children[0].childNodes.forEach(li=>{
+            if(!li.classList.contains("seized")){
+                matched = false;
+            }
+        }) 
+        if(matched){
+            child.remove();
+            prependNewLine();
+            score++;
+            scoreDisplay.innerText = score;
+        }
+    })
+    generateNewBlock()
+}
+function generateNewBlock(){
+
+    clearInterval(downInterval);
+    downInterval = setInterval(() => {
+        moveBlock('top', 1);
+    }, duration)
+
+    const blockArray = Object.entries(BLOCKS);
+    const randomIndex = Math.floor(Math.random() * blockArray.length)
+
+    MovingItem.type = "blockArray[randomIndex][0]"
+    MovingItem.top = 0;
+    MovingItem.left = 3;
+    MovingItem.direction = 0;
+    tempMovingItem = {...MovingItem};
+    renderBlocks()
 }
 function checkEmpty(target){
-    if(!target){
+    if(!target || target.classList.contains("seized")){
         return false;
     }
     return true;
@@ -93,6 +136,16 @@ function chageDirection(){
     direction === 3 ? tempMovingItem.direction =0 : tempMovingItem.direction += 1;
     renderBlocks()
 }
+function dropBlock(){
+    clearInterval(downInterval);
+    downInterval = setInterval(()=>{
+        moveBlock("top, 1")
+    }, 10)
+}
+function showGameoverText(){
+    gameText.style.display = "flex"
+}
+
 
 // event handling
 document.addEventListener("keydown", e=>{
@@ -109,7 +162,16 @@ document.addEventListener("keydown", e=>{
         case 38:
             chageDirection();
             break;
+        case 32:
+            dropBlock();
+            break;
         default:
             break;
     }
+})
+
+restartButton.addEventListener("click",()=>{
+    playground.innerHTML = "";
+    gameText.style.display = "none"
+    init()
 })
